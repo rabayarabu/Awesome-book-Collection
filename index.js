5589f07de5d1fec29b504f1a91ca9767e15182de
rabayarabu <rabu9149@gmail.com>
2023-05-03 16:16:41 +0600

    Add contact us section

--- index.js
+++ index.js
@@ -93,22 +93,39 @@ booksCollection.init();
 
 const list = document.querySelector('#book-list');
 const addNew = document.querySelector('#add-new-book');
+const contact = document.querySelector('#contact-us');
 const books = document.querySelector('.books');
 const form = document.querySelector('#form');
+const contactSection = document.querySelector('.contact-section');
 
 form.classList.add('hidden');
+contactSection.classList.add('hidden');
+books.classList.remove('hidden');
 addNew.style.textDecoration = 'underline';
 
 list.addEventListener('click', () => {
   list.style.textDecoration = 'underline';
   addNew.style.textDecoration = 'none';
+  contact.style.textDecoration = 'none';
   form.classList.add('hidden');
   books.classList.remove('hidden');
+  contactSection.classList.add('hidden');
 });
 
 addNew.addEventListener('click', () => {
   list.style.textDecoration = 'none';
   addNew.style.textDecoration = 'underline';
+  contact.style.textDecoration = 'none';
   form.classList.remove('hidden');
   books.classList.remove('hidden');
+  contactSection.classList.add('hidden');
+});
+
+contact.addEventListener('click', () => {
+  list.style.textDecoration = 'none';
+  addNew.style.textDecoration = 'none';
+  contact.style.textDecoration = 'underline';
+  form.classList.add('hidden');
+  books.classList.add('hidden');
+  contactSection.classList.remove('hidden');
 });
